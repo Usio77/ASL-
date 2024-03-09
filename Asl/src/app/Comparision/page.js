bddd7e76cf736927aeb3355a2b2111bd2f3dcd4b
Prabhas1225 <prabhasparajuli@gmail.com>
2024-03-09 10:16:22 +0545

    Final changes

--- Asl/src/app/Comparision/page.js
+++ Asl/src/app/Comparision/page.js
@@ -7,12 +7,12 @@ import ImageUploader from "./test";
 export default function Comparision() {
   return (  
 
-    <main className="h-screen bg-[#D8D8D8] text-black">
-    <div className=" pt-6 text-2xl font-bold pl-[450px] text-[#1d2342]"> Sign Language Detection Using YOLO </div>
+    <main className="h-screen bg-gradient-to-r from-[#D8D8D8] to-gray-800 text-black">
+    <div className=" text-4xl font-bold pl-[550px] pt-[100px] text-[#1d2342]"> Sign Language Detection Using YOLO </div>
       <div className="  pt-[50px] pl-[450px]">
 {/*      
      <VideoInput width={75} height={75} />  */}
-     <ImageUploader />
+     <ImageUploader className="p-[200px]" />
    </div>
     </main>
     
--- Asl/src/app/Comparision/test.js
+++ Asl/src/app/Comparision/test.js
@@ -64,9 +64,20 @@ function ImageUploader({ onUpload }) {
 
 
   return (
-    <div>
-      <input type="file" accept="video/*, image/*" onChange={handleFileChange} />
-      {!output && <button onClick={handleUpload} >{loading  ? <p>Loading.....</p>: <p>Upload</p>}</button>}
+    <div className=" pl-32">
+      <input type="file" accept="video/*, image/*" onChange={handleFileChange}/>
+      {!output && <button onClick={handleUpload} >{loading  ?
+      <div className=" transition-opacity ">
+      <div className=" flex flex-row animate-pulse w-60 h-12 pl-4 pt-1 bg-[#7366C0] rounded-xl"><div
+    class="inline-block h-6 w-6 mt-2 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+    role="status"></div><div className="pt-2 grow "><span className="-pl-3 mb-20 font-bold">Preparing Output </span></div></div>
+
+    {/* <div className=" p-11 w-80 ease-in-out">It might take a while</div> */}
+      </div>
+
+
+
+  : <div className=" text-xl font-medium bg-[#7366C0] w-32 h-10 rounded-xl -pl-32"><p className="  pt-1">Upload</p></div>}</button>}
       
       {/* {output && 
       <video width="320" height="320">
@@ -82,16 +93,20 @@ function ImageUploader({ onUpload }) {
      {/* <a href={"http://127.0.0.1:5500/yolov7/"+ output} target="_blank" download>
      Play Video
       </a> */}
-
-
-<div>
-  { output && <a href={"/exp/"+ output} download>
+      {/* <div className=" ml-[150px] mt-[45px] pl-11 pt-4 bg-[#7366C0] w-56 h-16 rounded-xl content-center items-center font-bold " >
+    <a >
     Download Ouptput
-      </a>}
-</div>
+      </a>
+</div> */}
 
-      
- </div>
+
+
+<div >
+  { output &&<div className="ml-[150px] mt-[45px] pl-11 pt-4 bg-[#7366C0] w-56 h-16 rounded-xl content-center items-center font-bold "><a href={"/exp/"+ output} download>
+    Download Ouptput
+      </a></div>}
+</div>   
+</div>
     
   );
 
